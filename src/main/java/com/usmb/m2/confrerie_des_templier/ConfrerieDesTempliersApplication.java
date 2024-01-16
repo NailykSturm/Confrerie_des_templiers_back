030f63f4635e5c76dd0eb603791377acc8ad39dc
Jules Finck <jules.finck@hotmail.fr>
2024-01-16 14:19:32 +0100

    init

---
+++ src/main/java/com/usmb/m2/confrerie_des_templier/ConfrerieDesTempliersApplication.java
@@ -0,0 +1,13 @@
+package com.usmb.m2.confrerie_des_templier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfrerieDesTempliersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfrerieDesTempliersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usmb/m2/confrerie_des_templier/ConfrerieDesTempliersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usmb.m2.confrerie_des_templier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfrerieDesTempliersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
